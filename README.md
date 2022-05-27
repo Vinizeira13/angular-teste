6f5af7b861c79e5adbf8f8e4d60c575c8f326f5a
Vinizeira13 <cbrviny13@gmail.com>
2022-05-27 12:53:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hrfwra
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hrfwra)
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+  <p> 
+  `,
+})
+export class MeuPrimeiroComponent {}
