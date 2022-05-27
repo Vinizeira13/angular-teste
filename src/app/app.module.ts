3b022c14b034eccd095291ec5a77929ba356fd3b
Vinizeira13 <cbrviny13@gmail.com>
2022-05-27 14:11:14 +0100

    3 files modified, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,15 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent,
     MeuPrimeiroComponent,
   ],
   bootstrap: [AppComponent],
